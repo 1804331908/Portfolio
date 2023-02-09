72f0dfe8541563ec90b11aab451c765d1318b057
Shivam Maheshwari <69828098+shivammaheshwari5@users.noreply.github.com>
2023-02-09 11:52:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 function sendMail(){
-    let params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    }
-    const serviceID = "service_1x2q2rs";
-    const templateID = "template_2p8ubgr";
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "shivammaheshwari503@gmail.com",
+    Password : "93B9A0E01851828946348DADB87A9DE493F4",
+    To : 'shivam8077990774@gmail.com',
+    From : "shivammaheshwari503@gmail.com",
+    Subject : "This is the subject",
+    Body : "Name:" + document.getElementById("name").value
+    + "<br> Email:" + document.getElementById("email").value
+    + "<br> Message:" + document.getElementById("message").value
+}).then(
+  message => alert(message + " " + "Message Send Successfully!")
+);
 
-    emailjs.send(serviceID, templateID, params).then((res) => {
-    alert("Your message has successfully sent!" + res.status);
-}).catch((err) => console.log(err));
-        
-    
 }
+
+
 
 var menu_bar = document.querySelector(".menubar");
 var main_div = document.querySelector(".nav-content");
@@ -20,12 +23,7 @@ var sidebar = document.querySelector("#navUl")
 var toggle =  document.querySelector("#toggle")
 
 menu_bar.addEventListener("click", () => togglebar());
-// document.onclick = function (e) {
-//   if(e.target.id !== "sidebar" && e.target.id !== "toggle" ){
-//     sidebar.classList.remove("active");
-//     toggle.classList.remove("active");
-//   }
-// }
+
 const togglebar = () => {
   main_div.classList.toggle("active");
 };
