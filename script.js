4ec31300c2c103dfd0ebfd84ca7df055f723fe0d
Shivam Maheshwari <69828098+shivammaheshwari5@users.noreply.github.com>
2023-02-11 11:51:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -19,11 +19,21 @@ function sendMail(){
 
 var menu_bar = document.querySelector(".menubar");
 var main_div = document.querySelector(".nav-content");
+console.log(main_div)
 var sidebar = document.querySelector("#navUl")
 var toggle =  document.querySelector("#toggle")
+var navLink = document.querySelector(".nav-link")
+console.log(navLink)
+
 
 menu_bar.addEventListener("click", () => togglebar());
 
 const togglebar = () => {
   main_div.classList.toggle("active");
 };
+
+
+function RemoveSidebar(){
+  main_div.classList.remove("active");
+}
+
